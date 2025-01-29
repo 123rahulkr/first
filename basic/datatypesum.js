7daf22e8c7cd70b0b676539b26a95ac6a62c5c2d
123rahulkr <rkr201759@gmail.com>
2025-01-29 10:32:07 +0000

    datatypesum

---
+++ basic/datatypesum.js
@@ -0,0 +1,19 @@
+//primitive
+
+//7 types: string,number,boolean,null,undefined,symbol,bigint
+
+//referrence(non primitve)
+//array,objects,functions
+
+let username;
+const id=symbol('123');
+const anotherid=symbol('123');
+console.log(id===anotherid);
+let myobj={
+    name:"rahul",
+    age:33,
+}
+a=6678767987789n;
+const myfunc= function(){
+        console.log(big int)
+}
